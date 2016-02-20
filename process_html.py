aafb81edea5e03006de392e1ae3f3c3e0b6dbdf8
Cameron Averill <cra2126@columbia.edu>
2016-02-20 16:50:35 -0500

    get the paragraphs containing terms

--- process_html.py
+++ process_html.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import os 
 
+#generate a beautiful soup file for each fil ein the myoutput directory
 file_names = []
 for (root, dirs, f) in os.walk('myoutput'):
 	for file in f:
@@ -8,7 +9,11 @@ for (root, dirs, f) in os.walk('myoutput'):
 			data = myfile.read()
 			file_names.append(data)
 
+#generates the divs of all paragraphs containing any term
 for f in file_names:
 	soup = BeautifulSoup(f, 'html.parser')
-	print(soup.prettify())
-
+	bold_term_tags = soup.findAll("span", attrs={'style': 'font-family: HIEILF+StoneSerif-Bold-20-0; font-size:9px'})
+	parent_tags = []
+	for t in bold_term_tags:
+		parent_tags.append(t.parent)
+		print parent_tags[len(parent_tags) - 1].get_text()
