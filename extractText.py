947270c4d08363f2db1dcec931150d9ccc289152
Serena Wong <serenaw@stanford.edu>
2016-02-20 19:21:57 -0500

    cleaned up output

--- extractText.py
+++ extractText.py
@@ -14,3 +14,10 @@ file.close()
 subprocess.call(["pdf2txt.py", "-o", "myfile.html", "-t", "html", "-p", pages, pdfFileName])
 
 subprocess.call(["rm", pdfFileName])
+
+subprocess.call(["mkdir", "myoutput"])
+subprocess.call(["mv", "myfile.html", "myoutput"])
+
+
+
+#http://mhs.marbleheadschools.org/teachers/heenan/002F66C7-011F79D6.84/Ch%2054%20Campbell%209th%20ed.pdf
